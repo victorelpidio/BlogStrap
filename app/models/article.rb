baa01a0e7ab92aa0c5012f2626510f12bc0a0e7b
victor elpidio <45489@tjce.jus.br>
2022-09-16 17:57:33 -0300

    criaçao do caterogy e suas configuraçoes

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,10 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true, length: {minimum: 10}
 
+  belongs_to :category
+
   paginates_per 3 #quantos registros por pagina// kaminari
 
   scope :desc_order, ->  { order(created_at: :desc) }
-  scope :without_highlights, -> (ids) { where("id NOT IN(#{ids})") }
+  scope :without_highlights, -> (ids) { where("id NOT IN(#{ids})") if ids.present? }
 end
