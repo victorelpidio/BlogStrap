17afb24d089b03d2adce73b81b2a0d1e5858af62
victor elpidio <45489@tjce.jus.br>
2022-12-15 15:18:21 -0300

    Configurando I18n - Pt 02

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def render_if(condition, template, record) #helper feito para renderizar uma artigo valido 
     render template, record if condition
   end
+
+  def handle_date(datetime)
+    #datetime.strftime('%e de %B, %Y') // sem o i18n
+    l(datetime, format: '%e de %B, %Y').capitalize
+  end
+
+  def sub_masked_email(email)
+    email.gsub(/(?<=.{2}).*@.*(?=\S{2})/, '****@****')
+  end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,2 @@
 module ArticlesHelper
-  def handle_date(datetime)
-    datetime.strftime('%e de %B, %Y')
-  end
 end
