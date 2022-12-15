f4439ee9ab41a76be03d187d8fb977ebfbf07e26
victor elpidio <45489@tjce.jus.br>
2022-12-15 11:43:47 -0300

    Configurando I18n - Pt 01

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'articles#index'
+  scope '(:locale)', locale: /pt-BR|en/ do 
+    devise_for :users
+    root 'articles#index'
+    
+    resources :articles do 
+      resources :comments, only: %i[create destroy] #para o comentario estar dentro do article
+    end
   
-  resources :articles do 
-    resources :comments, only: %i[create destroy] #para o comentario estar dentro do article
+    resources :categories, except: [:show]
   end
-
-  resources :categories, except: [:show]
 end
