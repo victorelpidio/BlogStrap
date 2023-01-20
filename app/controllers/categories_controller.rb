26cce6734ba5ad9fb4286a23f754c721000f250a
victor elpidio <45489@tjce.jus.br>
2023-01-20 10:21:55 -0300

    Completando I18n - PT 02

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,7 +25,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to categories_url, notice: "Category was successfully created." }
+        format.html { redirect_to categories_url, notice: t('app.create.success', model: Category.model_name.human)}
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to categories_url, notice: "Category was successfully updated." }
+        format.html { redirect_to categories_url, notice:  t('app.update.success', model: Category.model_name.human) }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -51,11 +51,11 @@ class CategoriesController < ApplicationController
   def destroy
     if @category.destroy
       respond_to do |format|
-        format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
+        format.html { redirect_to categories_url, notice:  t('app.destroy.success', model: Category.model_name.human) }
         format.json { head :no_content }
       end
     else
-      redirect_to categories_url, alert: @category.errors.messages[:base][0] # mensagem que nao permite apagar um categoru com um article dentro([:base][0] = pega a string)
+      redirect_to categories_url, alert: @category.errors.messages[:base][0] # mensagem que nao permite apagar um category com um article dentro([:base][0] = pega a string)
     end
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,14 @@ module ApplicationHelper
     render template, record if condition
   end
 
-  def handle_date(datetime)
+  def handle_date(value)
     #datetime.strftime('%e de %B, %Y') // sem o i18n
-    l(datetime, format: '%e de %B, %Y').capitalize
+    l(value, format: '%e de %B, %Y').capitalize
+  end
+
+  def handle_date_month(value)
+    #datetime.strftime('%e de %B, %Y') // sem o i18n
+    l(value.to_datetime, format: '%B, %Y').capitalize
   end
 
   def sub_masked_email(email)
